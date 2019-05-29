6c5da891c9855d0dc6c0c2a25cff70e15a7d086b
YukiHa111 <50425874+YukiHa111@users.noreply.github.com>
2019-05-29 18:57:01 -0400

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,176 @@
+<?php
+$catalog=[];
+//Books
+$catalog[101] = [
+ "title" => "The Little Prince",
+ "img" => "img/media/bookPrince.jpg",
+ "genre" => "Classic Literature",
+ "format" => "Paperback",
+ "year" => 2000,
+ "category" => "Books",
+ "authors" => [
+     "Antoine de Saint-Exupéry",
+     "1Richard Howard",
+ ],
+ "publisher" => "Mariner Books; 1st edition",
+ "isbn" => '978-0156012195'
+];
+
+$catalog[102] = [
+ "title" => "Chronicles of Narnia",
+ "img" => "img/media/bookNarnia.jpeg",
+ "genre" => "Fantasy-Magic Books",
+ "format" => "Paperback",
+ "year" => 2010,
+ "category" => "Books",
+ "authors" => [
+     "C. S. Lewis",
+ ],
+ "publisher" => "HarperCollins Narnia; Media Tie In edition",
+ "isbn" => '978-0061992889'
+];
+
+$catalog[103] = [
+ "title" => "Natsume's Book of Friends",
+ "img" => "img/media/bookNatsume.jpg",
+ "genre" => "Manga",
+ "format" => "Paperback",
+ "year" => 2010,
+ "category" => "Books",
+ "authors" => [
+     "Yuki Midorikawa",
+ ],
+ "publisher" => "VIZ Media LLC",
+ "isbn" => '978-1421532431'
+];
+
+$catalog[104] = [
+ "title" => "Herding Cats: A Sarah's Scribbles Collection",
+ "img" => "img/media/bookHerdingCats.jpg",
+ "genre" => "Art of Comics and Manga",
+ "format" => "Paperback",
+ "year" => 2018,
+ "category" => "Books",
+ "authors" => [
+     "Sarah Andersen",
+ ],
+ "publisher" => "Andrews McMeel Publishing",
+ "isbn" => '978-1449489786'
+];
+//Movies
+$catalog[201] = [
+ "title" => "How to Train Your Dragon: The Hidden World",
+ "img" => "img/media/movieDragon.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2019,
+ "category" => "Movies",
+ "director" => "Dean DeBlois",
+ "writers" => [
+     "Dean DeBlois",
+ ],
+ "stars" => [
+     "Jay Baruchel",
+     "America Ferrera",
+     "Cate Blanchett"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Harry Potter and the Half Blood Prince",
+ "img" => "img/media/movieHarry.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "David Yates",
+ "writers" => [
+     "J.K. Rowling",
+	 "Steve Kloves"
+ ],
+ "stars" => [
+     "Daniel Radcliffe",
+     "Rupert Grint",
+     "Emma Watson"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "AVENGERS: INFINITY WAR",
+ "img" => "img/media/movieAvengers.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => ["Joe Russo","Anthony Russo"],
+ "writers" => [
+     "Christopher Markus",
+	 "Stephen McFeely"
+ ],
+ "stars" => [
+     "Robert Downey Jr.",
+     "Chris Hemsworth",
+     "Scarlett Johansson"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Sherlock Holmes: A Game of Shadows",
+ "img" => "img/media/movieHolmes.jpg",
+ "genre" => "Mystery, Thrillers",
+ "format" => "DVD",
+ "year" => 2012,
+ "category" => "Movies",
+ "director" => "Guy Ritchie",
+ "writers" => [
+     "Michele Mulroney",
+	 "Kieran Mulroney"
+ ],
+ "stars" => [
+     "Robert Downey Jr.",
+     "Jude Law",
+     "Noomi Rapace"
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "Comme au premier jour",
+ "img" => "img/media/albumComme.png",
+ "genre" => "Instrumental",
+ "format" => "CD",
+ "year" => 2014,
+ "category" => "Music",
+ "artist" => "André Gagnon"
+];
+
+$catalog[302] = [
+ "title" => "Fearless Enhanced",
+ "img" => "img/media/albumStory.png",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 2008,
+ "category" => "Music",
+ "artist" => "Taylor Swift"
+];
+
+$catalog[303] = [
+ "title" => "ayumi hamasaki COUNTDOWN LIVE 2013-2014",
+ "img" => "img/media/albumpart.png",
+ "genre" => "J-Pop",
+ "format" => "CD",
+ "year" => 2014,
+ "category" => "Music",
+ "artist" => "Ayumi Hamasaki"
+];
+
+$catalog[304] = [
+ "title" => "Lala Hsu: 一愛難求",
+ "img" => "img/media/albumLala.png",
+ "genre" => "Pop",
+ "format" => "Single",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "LaLa Hsu"
+];
+?>
